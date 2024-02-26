578656003b5f50156a25dca4a53781be8becd2cf
hasnain <mhmdhasnain5@gmail.com>
2024-02-25 19:31:59 -0800

    responsive

---
+++ js/app.js
@@ -0,0 +1,13 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
+rightNav = document.querySelector('.rightNav');
+
+
+
+burger.addEventlistener('.click', ()=>{
+    rightNav.classlist.toggle('v-class-resp');
+    navList.classlist.toggle('v-class-resp');
+    navList.classlist.toggle('v-class-resp');
+}
+) 
